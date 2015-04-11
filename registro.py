f754704cebbf25b2177aa9744db31c378bcf34d7
Daniel Augusto <danisson10@gmail.com>
2015-04-11 17:31:34 -0300

    Atualizações para Python3 Basicamente suporte pra slices e mudando xrange pra range

--- registro.py
+++ registro.py
@@ -4,15 +4,26 @@ class Registro(object):
 	"""Registro 128 bytes"""
 	def __init__(self):
 		super(Registro, self).__init__()
-		self.vetor = array('B',(0 for _ in xrange(128)))
+		self.vetor = array('B',(0 for _ in range(128)))
 		self.dirty = False
 	def __getitem__(self, key):
-		if key > 128 or key < 0: raise Exception('Chave fora do vetor')
-		return self.vetor[key]
+		if isinstance(key,slice):
+			return self.vetor[key]
+		elif isinstance(key, int):
+			if key > 128 or key < 0: raise Exception('Chave fora do vetor')
+			return self.vetor[key]
+		else:
+			raise TypeError("Argumento invalido")
 	def __setitem__(self, key, value):
-		if key > 128 or key < 0: raise Exception('Chave fora do vetor')
-		self.vetor[key] = value
-		self.dirty = True
+		if isinstance(key,slice):
+			self.vetor[key] = value
+			self.dirty = True
+		elif isinstance(key, int):
+			if key > 128 or key < 0: raise Exception('Chave fora do vetor')
+			self.vetor[key] = value
+			self.dirty = True
+		else:
+			raise TypeError("Argumento invalido")
 	def __str__(self):
 		string = "["
 		for x in self.vetor:
